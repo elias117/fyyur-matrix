7ea6944cd45af8145fc70720462af824d271de87
Elias Zoghaib <elias.zoghaib@dal.ca>
2022-05-16 12:49:47 -0300

    Update config and app

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,4 +11,10 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+database_user = os.getenv("DATABASE_USER", "postgres")
+database_password = os.getenv("DATABASE_PASSWORD", "postgres")
+database_address = os.getenv("DATABASE_ADDRESS", "localhost:5432")
+database_name = os.getenv("DATABASE_NAME", "fyyur")
+SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://{}:{}@{}/{}".format(
+    database_user, database_password, database_address, database_name
+)
